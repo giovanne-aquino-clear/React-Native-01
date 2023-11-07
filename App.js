5a7dfb842261535caa821111ae2c8afbd0d805a2
Giovanne <giovanne.aquno.clear@gmail.com>
2023-11-07 12:34:46 -0300

    instagram page pt1

--- App.js
+++ App.js
@@ -1,58 +1,126 @@
+
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
+  Platform,
   StyleSheet,
-  FlatList,
-  Switch
-  } from 'react-native';
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
-  import {Picker} from '@react-native-picker/picker';
- import Slider from '@react-native-community/slider';
-class App extends Component{
+import Lista from './src/Lista';
+
+class App extends Component {
 
   constructor(props){
-    super(props)
-    this.state = { 
-      status: false
-    };
+    super(props);
+    this.state = {
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
+      ]
+     };
+ 
   }
 
-  render(){
-
-    return(
+  render() {
+    return (
       <View style={styles.container}>
+      
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/logo.png')}
+          style={styles.logo}
+          />
+        </TouchableOpacity>
 
-        <Switch 
-          value={this.state.status}
-          onValueChange={(valorSwitch)=> this.setState({status: valorSwitch})}
-        />
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/send.png')}
+          style={styles.send}
+          />
+        </TouchableOpacity>
+      </View>
 
-        <Text>
-        {(this.state.status) ? "Ativo" : "Inativo"}
-
-        </Text>
+      <FlatList
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item)=> item.id}
+      data={this.state.feed}
+      renderItem={ ({item}) => <Lista data={item} /> }
+      />
 
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    marginTop: 20,
-    backgroundColor: 'white',
-    alignItems:'center'
-    
+  container: {
+    flex: 1,
   },
-  bar:{
-    textAlign: 'center',
-    fontSize:20,
-    fontWeight: 'bold'
+  header:{
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1,
+  },
+  send:{
+    width: 23,
+    height: 23
   }
+
 });
 
 export default App;
-
-
---
+++ src/Lista/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+class Lista extends Component{
+  render(){
+    return(
+      <View>
+        <Text> Feed...</Text>
+      </View>
+    );
+  }
+}
+
+export default Lista;
