b1b738a97e638d6c855f4d6b09a5a6ea00a4f1a8
Giovanne <giovanne.aquno.clear@gmail.com>
2023-11-07 11:10:51 -0300

    uses pickle for selection menu

--- App.js
+++ App.js
@@ -6,43 +6,64 @@ import {
   FlatList
   } from 'react-native';
 
-import Pessoa from './src/Pessoas'
+  import {Picker} from '@react-native-picker/picker';
 
 class App extends Component{
 
   constructor(props){
     super(props);
-    this.state = {
-      feed:[
-        {id:'1', nome: 'Matheus', idade: 50, email: 'matheus@matheus.com'},
-        {id:'2', nome: 'Joao', idade: 22, email: 'joao@joao.com'},
-        {id:'3', nome: 'Henrique', idade: 39, email: 'henrique@henrique.com'},
-        {id:'4', nome: 'Paulo', idade: 15, email: 'paulo@paulo.com'},
-        {id:'5', nome: 'JOSE', idade: 12, email: 'jose@jose.com'},
+    this.state={
+      pizza: 0,
+      pizzas:[
+        {key: 1, nome: 'Strogonoff', valor: "35,90"},
+        {key: 2, nome: 'Carne Seca', valor: "45,90"},
+        {key: 3, nome: 'Calabresa', valor: "25,90"},
+        {key: 4, nome: 'Frango com Catupiry', valor: "38.90"},
       ]
-    };
+      
+    }
   }
 
   render(){
+
+    let pizzasItem = this.state.pizzas.map( (v, k)=> {
+      return <Picker.Item key={k} value={k} label={v.nome}/>
+    })
+
     return(
       <View style={styles.container}> 
+      <Text style={styles.logo}> Menu Pizza</Text>
 
-      <FlatList
-      data={this.state.feed}
-      keyExtractor={(item) => item.id}
-      renderItem={ ({item}) => <Pessoa data={item} /> }
-      />
+        <Picker
+          selectedValue={this.state.pizza}
+          onValueChange={ (itemValue, itemIndex) => this.setState({pizza: itemValue})}
+        >
+          {pizzasItem}
+        </Picker>
+
+      <Text style={styles.pizzas}> Voce escolheu: Pizza de {this.state.pizzas[this.state.pizza].nome}</Text>
+      <Text style={styles.pizzas}> R$ {this.state.pizzas[this.state.pizza].valor}</Text>
 
       </View>    
     );
   }
-
 }
+
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    marginTop: 20,
   },
- 
+  logo:{
+    textAlign: 'center',
+    fontSize:20,
+    fontWeight: 'bold'
+  },
+  pizzas:{
+    marginTop: 15,
+    fontSize: 20,
+    textAlign : 'center'
+  }
 });
 
 export default App;
